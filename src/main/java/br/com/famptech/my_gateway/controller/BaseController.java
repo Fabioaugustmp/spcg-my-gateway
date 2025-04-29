97a1be80b2ef761d9396b888a0d99a3d31c460b9
Fabio Augusto Marques Paula <fabioaugustomarquespaula@gmail.com>
2025-04-29 15:47:32 -0300

    feat: Adding weight to process

--- src/main/java/br/com/famptech/my_gateway/controller/BaseController.java
+++ src/main/java/br/com/famptech/my_gateway/controller/BaseController.java
@@ -1,5 +1,6 @@
 package br.com.famptech.my_gateway.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -13,9 +14,15 @@ public class BaseController {
         return Mono.just("fallback system is down!");
     }
 
-    @RequestMapping("/notifications")
+    @RequestMapping(value = "/notifications", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<String> canaryMono() {
-        return Mono.just("This is a canary response!!!");
+        return Mono.just("""
+                {
+                    "id": 100,
+                    "orderId": "69110be9-d8af-415f-b7b1-a89adaa7cee0",
+                    "email": "fabio.paula@gmail.com",
+                    "createdAt": "2025-04-24T13:55:50"
+                }""");
     }
 
 }
